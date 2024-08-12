d14285a58f12816b606b82ce426d6d6be56bee9c
Suppalerk Petpon <128837099+Suppalerkjrnon@users.noreply.github.com>
2024-08-12 17:50:45 +0700

    Update expense_log.py
    Update source code

--- expense_log.py
+++ expense_log.py
@@ -11,6 +11,7 @@ from streamlit_extras.metric_cards import style_metric_cards
 import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #################### Session State ####################
 
@@ -20,14 +21,25 @@ if 'gsheets' not in st.session_state:
 if 'generate_report' not in st.session_state:
     st.session_state.generate_report = False
     
+if 'show_dataframe' not in st.session_state:
+    st.session_state.show_dataframe = False
+    
 if 'existing_data' not in st.session_state:
     st.session_state.existing_data = None
     
 if 'expense_log' not in st.session_state:
     st.session_state.df = None
     
+if 'current_expense_log' not in st.session_state:
+    st.session_state.current_expense_log = None
+    
 if 'previous_expense_log' not in st.session_state:
     st.session_state.previous_expense_log = None
+    
+if 'budget_frame' not in st.session_state:
+    st.session_state.budget_frame = None
+    
+#################### Expense Log Class ####################
 
 class Expense_Log:
     def __init__(self):
@@ -37,7 +49,7 @@ class Expense_Log:
             st.session_state.gsheets = True
             
             # Read data from Google Sheets
-            self.existing_data = self.conn.read(spreadsheet= 'Replace with your Google Sheet shared link url')
+            self.existing_data = self.conn.read(spreadsheet= 'your google sheet share link')
             st.session_state.existing_data = self.existing_data
             
             #Convert Existing Data to DataFrame
@@ -49,7 +61,7 @@ class Expense_Log:
             self.df['year'] = self.df['year'].astype(str)
             self.df['year'] = self.df['year'].str.replace(',', '')
             self.df['date'] = self.df['date'].dt.strftime('%Y-%m-%d')
-
+            
         except Exception as e:
             st.error(f"Error: {e}")
             
@@ -58,6 +70,13 @@ def main():
     st.divider()
     expense_log = Expense_Log()
     st.session_state.expense_log = expense_log
+    
+    budget_frame = pd.DataFrame({
+        'type': ['transportation_budget', 'food_budget'],
+        'amount': [2400, 2500]
+    })
+    
+    st.session_state.budget_frame = budget_frame
     
     selector_month_year_col = st.columns(2)
 
@@ -74,23 +93,31 @@ def main():
         
             filtered_expense_log_df = expense_log.df[expense_log.df['month'] == selected_month]
             filtered_expense_log_df = filtered_expense_log_df.reset_index(drop=True)
-
+            
+            #Save to session state
+            st.session_state.current_expense_log = filtered_expense_log_df
+                
             #Create Previous Month DataFrame for Comparison
             previous_month = selected_month - 1
             filtered_previous_expense_log_df = expense_log.df[expense_log.df['month'] == previous_month]
+            
+            #Save to session state
             st.session_state.previous_expense_log = filtered_previous_expense_log_df
+        
+            if st.session_state.current_expense_log is not None:
+                st.dataframe(st.session_state.current_expense_log, width=1000)
+                st.session_state.show_dataframe = True
             
-            st.dataframe(filtered_expense_log_df, width=1000)
-            
-            #Generate Report Button
-            generate_report = st.button("Generate Report")
-            
-            if generate_report:
-                st.session_state.generate_report = True
-                # Summary the total cost
-                #Format the cost column to 2 decimal places
-                filtered_expense_log_df['cost'] = filtered_expense_log_df['cost'].apply(lambda x: round(x, 2))
-                #Comparison total sum of cost between filtered_expense_log_df and filtered_previous_expense_log_df
+
+            if not st.session_state.generate_report:
+                #Generate Report Button
+                generate_report = st.button("Generate Report")
+                if generate_report:
+                    st.session_state.generate_report = True
+                    st.session_state.show_dataframe = False
+                    
+            #After Generate Report Button is clicked
+            if st.session_state.generate_report:
                 if filtered_previous_expense_log_df.empty:
                     st.warning("No data available for comparison.")
 
@@ -98,46 +125,100 @@ def main():
                     current_total = filtered_expense_log_df['cost'].sum()
                     previous_total = filtered_previous_expense_log_df['cost'].sum()
                     
-                    #Format the total cost to 2 decimal places and delete comma
-                    formatted_current_total = int(abs(current_total))
-                    formatted_previous_total = int(abs(previous_total))
+                    #add, to the cost column and no decimal places
+                    formatted_current_total = "{:,.0f}".format(current_total)
+                    
+                    # formatted_current_total = int(current_total)
+                    formatted_previous_total = "{:,.0f}".format(previous_total)
+                    
+                    delta_cost = int(previous_total - current_total)
+                    
+                    #Transaction count
+                    current_transaction_count = filtered_expense_log_df.shape[0]
+                    previous_transaction_count = filtered_previous_expense_log_df.shape[0]
+                    
+                    delta_transaction_count = int(current_transaction_count - previous_transaction_count)
 
                 #Visualization Column Header
                 st.write("# Visualization")
                 st.divider()
                 #Metric value column
                 
-                metric_col = st.columns(2)
+                metric_col = st.columns(3)
                 
                 with metric_col[0]:
-                    st.metric(label="Total Expense", value=formatted_current_total, delta=formatted_previous_total - formatted_current_total)
-                    
-                    style_metric_cards(border_left_color="#672E6D")
-                    
-                    st.success(f"Total Expense: {filtered_expense_log_df['cost'].sum()} Bath")
+                    st.metric(label="Total Expense", value=formatted_current_total, delta=delta_cost)
+                    st.info(f"Total Expense: {int(filtered_expense_log_df['cost'].sum())} Bath")
                     
                     #Logic statement to compare the total cost between current and previous month
                     if previous_total > current_total:
-                        st.success(f"Total Expense Decreased by {formatted_previous_total - formatted_current_total} Bath")
+                        st.success(f"Total Expense Decreased by {abs(int(previous_total - current_total))} Bath")
                     elif previous_total < current_total:
-                        st.error(f"Total Expense Increased by {formatted_previous_total - formatted_current_total} Bath")
+                        st.error(f"Total Expense Increased by {abs(int(previous_total - current_total))} Bath") 
                     else:
-                        st.success("Total Expense Remained the Same")  
+                        st.success("Total Expense Remained the Same")
+                
+                with metric_col[1]:
+                    st.metric(label="Number of Transactions", value=current_transaction_count, delta=delta_transaction_count)
+                    st.info(f"Number of Transactions: {filtered_expense_log_df.shape[0]}")
+                    
+                    if previous_transaction_count > current_transaction_count:
+                        st.success(f"Number of Transactions Decreased by {abs(int(previous_transaction_count - current_transaction_count))}")
+                        
+                    elif previous_transaction_count < current_transaction_count:
+                        st.error(f"Number of Transactions Increased by {abs(int(previous_transaction_count - current_transaction_count))}")
+                        
+                    else:
+                        st.success("Number of Transactions Remained the Same")
+                        
+                with metric_col[2]:
+                    st.metric(label="Budget", value=budget_frame['amount'].sum(), delta=-int(current_total - budget_frame['amount'].sum()))
+                    st.info(f"Budget: {budget_frame['amount'].sum()} Bath")
+                    
+                    if current_total > budget_frame['amount'].sum():
+                        st.error(f"Budget Exceeded by {abs(int(current_total - budget_frame['amount'].sum()))} Bath")
+                        
+                    elif current_total < budget_frame['amount'].sum():
+                        st.success(f"You save money: {abs(int(current_total - budget_frame['amount'].sum()))} Bath")
+                        
+                    else:
+                        st.success("Budget Spent as Planned")
+                    
+                style_metric_cards(border_left_color="#672E6D")
+                
+                st.divider()
                 
                 #Visualization column
-                visualzation_col = st.columns(2)
+                visualzation_col = st.columns([5, 0.5])
                 
                 with visualzation_col[0]:
                     pie_source = filtered_expense_log_df[filtered_expense_log_df['cost'].notnull()]
-                    fig_pie = px.pie(pie_source, values='cost', names='type', title = 'Expense Proportion by Type')
-                    st.plotly_chart(fig_pie, use_container_width=True)
+                    pie_source = pie_source.groupby(['type'])['cost'].sum().reset_index()
+                    fig_pie = go.Figure(data=[go.Pie(labels=pie_source['type'], values=pie_source['cost'], hole=0.3)])
+                    
+                    # Update the layout to format numbers consistently
+                    fig_pie.update_traces(
+                        textinfo='label+percent+value', 
+                        texttemplate='%{value:,.0f}',   
+                    )
+                    
+                    fig_pie.update_layout(
+                        title='Expense Distribution by Type',
+                        
+                    )
+                    
+                    st.plotly_chart(fig_pie, use_container_width=True, theme='streamlit')
                     
                 with visualzation_col[1]:
                     bar_source_current = filtered_expense_log_df[filtered_expense_log_df['cost'].notnull()].groupby(['month'])['cost'].sum().reset_index()
-                    bar_source_previous = filtered_previous_expense_log_df[filtered_previous_expense_log_df['cost'].notnull()].groupby(['month'])['cost'].sum().reset_index()
-                    delta_cost = abs(bar_source_current['cost'].iloc[-1] - bar_source_previous['cost'].iloc[-1])
-                    formatted_delta_cost = int(delta_cost)
+                    bar_source_current['cost'] = bar_source_current['cost'].apply(lambda x: int(x))
                     
+                    bar_source_previous = filtered_previous_expense_log_df[filtered_previous_expense_log_df['cost'].notnull()].groupby(['month'])['cost'].sum().reset_index()
+                    bar_source_previous['cost'] = bar_source_previous['cost'].apply(lambda x: int(x))
+                    
+                    delta_cost = abs(int(bar_source_current['cost'].iloc[-1] - bar_source_previous['cost'].iloc[-1]))
+                    formatted_delta_cost = f'{int(abs(delta_cost)):,}'
+
                     # Coordinate of the line to connect the cost between current and previous month
                     x_coords = [bar_source_current['month'].iloc[-1], bar_source_previous['month'].iloc[-1]]
                     y_coords = [bar_source_current['cost'].iloc[-1], bar_source_previous['cost'].iloc[-1]]
@@ -150,26 +231,25 @@ def main():
                     fig_bar.add_trace(go.Bar(
                         x=bar_source_current['month'],
                         y=bar_source_current['cost'],
-                        text=bar_source_current['cost'],
+                        text=bar_source_current['cost'].apply(lambda x: f'{x:,}'),
                         name='Current Cost',
                         marker_color='blue'
                     ))
                     # Add previous costs bar trace
                     fig_bar.add_trace(go.Bar(
                         x=bar_source_previous['month'],
-                        y=bar_source_previous['cost'],
-                        text=bar_source_previous['cost'],
+                        y=bar_source_previous['cost'].apply(lambda x: round(x, 2)),
+                        text=bar_source_previous['cost'].apply(lambda x: f'{x:,}'),
                         name='Previous Cost',
                         marker_color='red'
                     ))
-                    
                     
                     #Add line to connect the cost between current and previous month, and add the text to show the difference cost between the month
                     fig_bar.add_trace(go.Scatter(
                         x= [x_coords[0], mid_x, x_coords[1]],
                         y= [y_coords[0], mid_y, y_coords[1]],
                         mode='lines',
-                        name='Connection',
+                        name='Difference',
                         textposition='top center',
                         line=dict(color='green', width=2, dash='dash')
                     ))
@@ -203,27 +283,36 @@ def main():
                         height=500
                     )
                     
-                    # Plot the chart using Streamlit
+                    
                     st.plotly_chart(fig_bar)
-                    
-                    #Visualization Column2
-                    
-                visualzation_col2 = st.columns(2)
                 
-                with visualzation_col2[0]:
-                    fig_line = px.line(filtered_expense_log_df, x='date', y='cost', title='Expense Frequency by Date')
-                    fig_line.update_layout(
-                        xaxis_title='Date',
-                        yaxis_title='Cost',
-                        width=1000,
-                        height=500
-                    )
-                    
-                    st.plotly_chart(fig_line)
+                ### Histogram ###
+                fig = px.histogram(
+                    filtered_expense_log_df,
+                    x='date',  
+                    nbins=30,  
+                    title='Expense Frequency by Date', 
+                    color_discrete_sequence=['skyblue']
+                )
+
+                fig.update_layout(
+                    title_text='Expense Frequency by Date', 
+                    title_x=0.5,  
+                    xaxis_title='Date',
+                    yaxis_title='Frequency',
+                    template='plotly_white',  
+                    autosize=True,
+                    width=1200,
+                    height=500,
+                    margin=dict(l=50, r=50, t=50, b=50) 
+                )
+
+                fig.update_traces(marker_color='#0077B6') 
+                
+                st.plotly_chart(fig)                
 
         else:
             st.error("Failed to load data.")
 
 if __name__ == "__main__":
     main()
-
